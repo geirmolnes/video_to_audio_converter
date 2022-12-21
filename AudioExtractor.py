34eed6a78761ff2e16d7b0e3cfbcc1284d275de5
Geir Molnes <g.molnes@gmail.com>
2022-12-21 10:17:29 +0100

    Add type hints

--- AudioExtractor.py
+++ AudioExtractor.py
@@ -5,7 +5,7 @@ import time
 
 
 class AudioExtractor:
-    def __init__(self, video_dir, audio_dir):
+    def __init__(self, video_dir: str, audio_dir: str) -> None:
         self.video_dir = video_dir
         self.audio_dir = audio_dir
         self.logger = logging.getLogger(__name__)
@@ -24,22 +24,22 @@ class AudioExtractor:
         # add the file handler to the logger
         self.logger.addHandler(fh)
 
-    def get_created_time(self, video_path):
+    def get_created_time(self, video_path: str) -> str:
         modified_time = os.path.getmtime(video_path)
         # convert to strftime
         return time.strftime("%d-%m-%Y %H:%M", time.localtime(modified_time))
 
-    def get_file_name(self, video_path):
+    def get_file_name(self, video_path: str) -> str:
         video_id = video_path.split("#")[1].replace(".mp4", "")
         created_time = self.get_created_time(video_path)
         return f"{created_time} {video_id}.mp3"
 
-    def is_converted(self, video_path):
+    def is_converted(self, video_path: str) -> bool:
         file_name = self.get_file_name(video_path)
         audio_path = os.path.join(self.audio_dir, file_name)
         return os.path.exists(audio_path)
 
-    def get_audio_from_video(self):
+    def get_audio_from_video(self) -> None:
         video_files = os.listdir(self.video_dir)
 
         for video in video_files:
